adb52b915d8ccba6d6276514665105cf8fa5a5ad
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-04-22 13:59:18 +0200

    base html y variables en el js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import "./style.css";
 
+const pronoun = ["tech", "next", "go", "vibe"];
+const adj = ["smart", "cloud", "nova", "data"];
+const noun = ["hub", "system", "space", "spot"];
+const ext = [".com", ".io", ".xyz", ".net"];
+
 window.onload = () => {
   //write your code here
 
