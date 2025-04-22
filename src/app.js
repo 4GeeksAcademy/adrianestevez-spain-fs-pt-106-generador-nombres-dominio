41195ab88908b1a544e6942f039f29b887c2fc5d
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-04-22 15:36:03 +0200

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
 import "./style.css";
 
+const cajaDominios = document.querySelector("#listado-dominios");
+
 const pronoun = ["tech", "next", "go", "vibe"];
 const adj = ["smart", "cloud", "nova", "data"];
 const noun = ["hub", "system", "space", "spot"];
 const ext = [".com", ".io", ".xyz", ".net"];
 
-window.onload = () => {
-  //write your code here
+const newDomain = [];
+const domainList = [];
 
+const genDomain = (arr, arr1, arr2, arr3) => {
+  for (let p of arr) {
+    newDomain.push(p); // the
+
+    for (let a of arr1) {
+      newDomain.push(a); // the, smart
+
+      for (let n of arr2) {
+        newDomain.push(n); // the, smart, hub
+
+        for (let e of arr3) {
+          newDomain.push(e); // the, smart, hub, .com
+          domainList.push(newDomain.join(""));
+          newDomain.pop(); // the, smart, hub
+        }
+
+        newDomain.pop(); // the, smart
+      }
+
+      newDomain.pop(); // the
+    }
+
+    newDomain.pop(); // [] 
+  }
+  return domainList;
+}
+
+genDomain(pronoun, adj, noun, ext);
+
+console.log(domainList);
+
+const html = domainList.map((dominio) => {
+  return `<li>${dominio}</li>`;
+});
+
+
+window.onload = () => {
+  cajaDominios.innerHTML = html.join("");
 };
